be57ff224487dfa8a4f9282c7b60cf76410c5c5d
Lautaro Pontoriero <l.pontoriero@yahoo.com>
2022-09-05 16:36:05 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -6,16 +6,22 @@ debugger
     alert("Hola " + nombre)
 
 function credito(prestamo, interes, cuotas) {
+    if(prestamo > 300000){
     alert("El monto a devolver es de $" + prestamo * interes);
     alert("El valor de cada una de las cuotas es de $" + (prestamo * interes) / cuotas);
  
     let resultado = 0;
     console.log(resultado);
     return resultado;
-    
+} else {
+    alert("Lo siento, el monto ingresado no es supera el mínimo para concretar el préstamo.")
 }
-let prestamo = parseFloat(prompt("Ingrese el monto que desea recibir como préstamo: "));
+}
+alert("RECUERDE QUE EL MONTO MINIMO DEBE SER MAYOR A $300.000");
 alert("El interés que se sumará en el monto a devolver es del 35%");
+let prestamo = parseFloat(prompt("Ingrese el monto que desea recibir como préstamo: "));
+
+
 interes = 1.35;
 let cuotas = prompt("Ingrese la cantidad de cuotas que desea para pagar su préstamo: ");
 
